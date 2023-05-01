78db677222225ba55c6e1d6aa5b81d4cbd6642f4
LaPandilla25 <NXCA060704HMCVLNA0@btgem.mx>
2023-05-01 14:13:20 -0600

    Ixora

--- js/indejs2.js
+++
@@ -1,6 +0,0 @@
-function closeDropdown() {
-    setTimeout(function () {
-        $(".dropdown .dropdown-content").removeClass("show");
-        $(".dropdown .caret-down").removeClass("rotate");
-    }, 250);
-}
--- js/indexjs1.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    $('.dropdown').click(function () {
-        $('.dropdown .caret-down').toggleClass('rotate');
-    });
-});
